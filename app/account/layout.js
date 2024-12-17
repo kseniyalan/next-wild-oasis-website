539f196a0984394ab8dab883f9ad1bb49bc286fc
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-12-17 22:19:52 +0100

    Account section

---
+++ app/account/layout.js
@@ -0,0 +1,11 @@
+import SideNavigation from "@/app/_components/SideNavigation";
+
+// Nested layout component
+export default function Layout({ children }) {
+  return (
+    <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+      <SideNavigation />
+      <div className="py-1">{children}</div>
+    </div>
+  );
+}
