cdd96f8e17f9a258f53f2df102089a9eb03950b8
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-12-17 20:59:30 +0100

    Layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import Header from "@/app/_components/Header";
 
 import "@/app/_styles/globals.css";
 
@@ -22,18 +21,18 @@ export const metadata = {
     "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
