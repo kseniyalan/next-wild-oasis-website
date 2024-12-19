bda0558957c0a2c3c8368c79ab604d2572b33de4
Kseniia Lanetskaia <kseniia.lanetskaia@jump-soft.com>
2024-12-19 16:44:07 +0100

    Cache and others

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,13 +1,28 @@
 import Image from "next/image";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 
 // Dynamic metadata
 // Must be named `generateMetadata`
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
+}
+
+// This function will be called at build time
+// // Returns the list of all cabin ids
+// Then, Next.js will generate a page for each cabin as static page
+// And therefore the page will be pre-rendered and will be faster
+// On this page dynamic data can be fetched using the `params` prop and 
+// dynamic param is cabinId
+// So we want to get all possible cabin ids
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
