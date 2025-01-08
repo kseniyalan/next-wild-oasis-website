bf4d4e010ff7ff7450e3dee12a723e11fc5c3542
Kseniia Lanetskaia <kseniia.lanetskaia@jump-soft.com>
2025-01-08 22:32:12 +0100

    Filter with URL

---
+++ app/_components/Filter.js
@@ -0,0 +1,70 @@
+"use client";
+// Client component
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+function Filter() {
+  // We use the useSearchParams hook to get the current search params
+  //Custom Next.js hook
+  const searchParams = useSearchParams();
+  // Other custom hook to implement programmatic navigation in Next.js
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // We get the current filter from the search params
+  const activeFilter = searchParams.get("capacity") ?? "all";
+
+  // Here we create search params and replace the current URL with the new one
+  function handleFilter(filter) {
+    const params = new URLSearchParams(searchParams);
+    params.set("capacity", filter);
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+  }
+
+  return (
+    <div className="border border-primary-800 flex">
+      <Button
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        All cabins
+      </Button>
+      <Button
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        2&mdash;3 guests
+      </Button>
+      <Button
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        4&mdash;7 guests
+      </Button>
+      <Button
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        8&mdash;12 guests
+      </Button>
+    </div>
+  );
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      className={`px-5 py-2 hover:bg-primary-700 ${
+        filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+      }`}
+      onClick={() => handleFilter(filter)}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Filter;
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
-import CabinList from "@/app/_components/CabinList";
-import Spinner from "@/app/_components/Spinner";
-
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 3600;
 // export const revalidate = 15;
@@ -10,7 +10,9 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -25,8 +27,15 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      {/* Suspense needs this key prop to re-render the component when the filter changes */}
+      {/* This way, we can show a loading spinner while the new data is being fetched */}
+      {/* Without the key prop, the component would not re-render */}
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
