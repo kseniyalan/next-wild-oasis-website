a227149ed482ddd424bdd848be55683562a1da5a
Kseniia Lanetskaia <kseniia.lanetskaia@jump-soft.com>
2025-01-09 22:31:07 +0100

    Next custom server endpoint

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,20 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+// It is a custom server route - API endpoint
+// This one will return data for a specific cabins
+export async function GET(request, { params }) {
+  const { cabinId } = params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+
+    return Response.json({ cabin, bookedDates });
+  } catch {
+    return Response.json({ message: "Cabin not found" });
+  }
+}
+
+// export async function POST() {}
